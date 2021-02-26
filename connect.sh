72eab664e92437a2d002deb98202d65f2a394f5d
Brian O'Connor <briandoconnor@gmail.com>
2021-02-25 20:46:54 -0800

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker exec -it `docker ps | grep terra-dataset-size-survey_py-dev | awk '{print $1}'` /bin/bash
+docker exec -it `docker ps | grep ncpi-dataset-size-survey_py-dev | awk '{print $1}'` /bin/bash
