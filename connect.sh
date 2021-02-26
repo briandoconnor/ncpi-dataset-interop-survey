1f582243305e0961004f24d0811a09f1e3a92a3f
Brian O'Connor <briandoconnor@gmail.com>
2021-02-25 20:49:04 -0800

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker exec -it `docker ps | grep ncpi-dataset-size-survey_py-dev | awk '{print $1}'` /bin/bash
+docker exec -it `docker ps | grep ncpi-dataset-interop-survey_py-dev | awk '{print $1}'` /bin/bash
